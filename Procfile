6626707bfb2fec444df5db36d751ca12059bb759
mdimranakhter <mdimranakhter998@gmail.com>
2022-02-07 23:13:00 +0530

    seven

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: waitress-serve --port=$PORT college.wsgi:application
--- college/settings.py
+++ college/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-j2!f()0h)@(nm*&$2cb_p!(rtp6xh(emfo)snpfa%*@hp8p&0-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["sciencecollege.herokuapp.com","127.0.0.1"]
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -41,8 +41,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.middleware.security.SecurityMiddleware',    
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
